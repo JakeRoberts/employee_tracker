ccfee3049c256a305196c1f61a745291d5c511a6
Rob <r.jacobs790@gmail.com>
2021-04-24 16:58:11 -0400

    updated index w/ view

--- index.js
+++ index.js
@@ -1,12 +1,59 @@
 const orm = require('./orm');
-
+const inquirer = require("inquirer");
 const seeTable = require('console.table');
 
-orm.getDepartments(console.table);
+const start = () => {
+    console.log("Welcome to Employee Tracker");
+    menu ();
+};
 
-// creating data in the table
-// orm.addDepartment({name:"IT"}, console.log);
-// orm.getDepartments(console.table);
-// orm.updateDepartment({name:"R&D"}, 1, console.log);
-orm.deleteDepartment(1, console.log);
-orm.getDepartments(console.table);
+const finish = () => {
+    console.log("Thank you for using Employee Tracker");
+};
+
+const menu = () => {
+    inquirer
+    .prompt([
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            name: 'option',
+            choices: ['View Employees', 'View Roles', 'View Departments', 'Exit'],
+        },
+    ])
+    .then (response => {
+        switch (response.option) {
+            case "View Employees": viewEmployees();
+            break;
+            case "View Roles": viewRoles();
+            break;
+            case "View Departments": viewDepartments();
+            break;
+            case "Exit": finish();
+            break;
+        }
+    });
+};
+
+const viewEmployees = () => {
+    orm.getEmployees(response => {
+        console.table(response);
+        menu();
+    });
+};
+
+const viewRoles = () => {
+    orm.getRoles(response => {
+        console.table(response);
+        menu();
+    });
+};
+
+const viewDepartments = () => {
+    orm.getDepartments(response => {
+        console.table(response);
+        menu();
+    });
+};
+
+start();
