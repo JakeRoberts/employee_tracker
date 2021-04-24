e0c5ea5d93988e9e97a68f48c9c3e2344247927d
Rob <r.jacobs790@gmail.com>
2021-04-24 16:23:22 -0400

    added select all insert into to  orm.js.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const orm = require('./orm');
+
+const seeTable = require('console.table');
+
+// orm.getDepartments(console.table);
+
+// creating data in the table
+// orm.addDepartment({name:"IT"}, console.log);
+// orm.getDepartments(console.table);
--- orm.js
+++ orm.js
@@ -1,1 +1,53 @@
 const db = require('./db');
+
+const getEmployees = (cb) => {
+    const sql = "SELECT * FROM employee";
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        cb(result);
+    });
+};
+
+const getRoles = (cb) => {
+    const sql = "SELECT * FROM role";
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        cb(result);
+    });
+};
+
+const getDepartments = (cb) => {
+    const sql = "SELECT * FROM department";
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        cb(result);
+    });
+};
+
+const addEmpolyee = (data, cb) => {
+    const sql = `Insert Into employee (first_name, last_name, role_id, manager_id) values ("${data.first_name}", "${data.last_name}", "${data.role_id}", "${data.manager_id}")`;
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        cb(result);
+    });
+}
+
+const addRole = (data, cb) => {
+    const sql = `Insert Into role (title, salary, department_id) values ("${data.title}", "${data.role}", "${data.department_id}")`;
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        cb(result);
+    });
+}
+
+const addDepartment = (data, cb) => {
+    const sql = `Insert Into department (name) values ("${data.name}")`;
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        cb(result);
+    });
+}
+
+module.exports = {
+    getEmployees, getRoles, getDepartments, addEmpolyee, addRole, addDepartment
+};
